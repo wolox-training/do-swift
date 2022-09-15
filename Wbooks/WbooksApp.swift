36af4da96745a7590c2a4f969070d9865b918ee7
dezbrid <dezbrid@gmail.com>
2022-09-15 15:12:05 -0500

    Initial Commit

---
+++ Wbooks/WbooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WbooksApp.swift
+//  Wbooks
+//
+//  Created by Daniel Ordoñez Arce on 15/09/22.
+//
+
+import SwiftUI
+
+@main
+struct WbooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
